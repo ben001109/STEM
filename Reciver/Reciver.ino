0f7157c9a501c71fa44c706069067da75231a9a8
MadDog <jh3420570@gmail.com>
2024-01-03 18:30:12 +0800

    add python support for RPI(repair the ESP32's code)

--- Reciver/Reciver.ino
+++ Reciver/Reciver.ino
@@ -1,86 +1,67 @@
-#include <WiFi.h>
-#include <WebServer.h>
-#include <SPI.h>
 #include <LoRa.h>
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
 
-// 定義ESP32-WROOM-DA的LoRa連接引腳
-#define ss 5    // 定義 SS 引腳為 GPIO5
-#define rst 14  // 定義 RST 引腳為 GPIO14
-#define dio0 2  // 定義 DIO0 引腳為 GPIO2
+#define BLYNK_TEMPLATE_ID "TMPL6oTC0xrt5"
+#define BLYNK_DEVICE_NAME "Drowning Detecting"
 
-String receivedLat, receivedLon, receivedStatus;
-WebServer server(80);
+#define ss 10
+#define rst 9
+#define dio0 7
+
+const char* ssid = "你的WiFi名稱";
+const char* password = "你的WiFi密碼";
+
+
+BlynkTimer timer;
 
 void setup() {
-  Serial.begin(115200);
-  // 設置ESP32為WiFi接入點
-  WiFi.softAP("LoRa_Receiver", "12345678");
-  Serial.println("WiFi AP Started");
-  Serial.println(WiFi.softAPIP());
+    Serial.begin(9600);
+    LoRa.setPins(ss, rst, dio0);
+    if (!LoRa.begin(868E6)) {
+        Serial.println("Starting LoRa failed!");
+        while (1);
+    }
+    Serial.println("LoRa Initialized");
 
-  // 初始化LoRa
-  LoRa.setPins(ss, rst, dio0);
-  if (!LoRa.begin(868E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
-  Serial.println("LoRa Initialized...");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
 
-  // 設置Web服務器的路由
-  server.on("/", HTTP_GET, handleRoot);
-  server.on("/data", HTTP_GET, sendData);
-  server.begin();
+    Blynk.begin(BLYNK_TEMPLATE_ID, BLYNK_DEVICE_NAME, ssid, password);
 }
 
 void loop() {
-  // 處理LoRa接收
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    String receivedData = "";
-    while (LoRa.available()) {
-      receivedData += (char)LoRa.read();
+    Blynk.run();
+    timer.run();
+    
+    // 檢查是否有來自LoRa的數據
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) {
+        String receivedData = "";
+        while (LoRa.available()) {
+            receivedData += (char)LoRa.read();
+        }
+        Serial.println("Received: " + receivedData);
+        processReceivedData(receivedData);
     }
-    Serial.println("Received: " + receivedData);
-    parseReceivedData(receivedData);
-  }
-  // 處理HTTP請求
-  server.handleClient();
 }
 
-void parseReceivedData(String data) {
-  // 解析從LoRa收到的數據
-  int latIndex = data.indexOf("LAT:");
-  int lonIndex = data.indexOf(",LONG:");
-  int statusIndex = data.indexOf(",STATUS:");
- 
-  if (latIndex != -1 && lonIndex != -1 && statusIndex != -1) {
-    receivedLat = data.substring(latIndex + 4, lonIndex);
-    receivedLon = data.substring(lonIndex + 6, statusIndex);
-    receivedStatus = data.substring(statusIndex + 8);
-  }
-}
-
-void handleRoot() {
-  // 生成並發送HTML頁面
-  String html = R"html(
-    <html>
-        <head>
-            // 省略的樣式和JavaScript代碼
-        </head>
-        <body onload="fetchData()">
-            <h1>LoRa Geo Fencing</h1>
-            <div class="data-box" id="latitude">Latitude: </div>
-            <div class="data-box" id="longitude">Longitude: </div>
-            <div class="data-box" id="status">Status: </div>
-        </body>
-    </html>
-    )html";
- 
-  server.send(200, "text/html", html);
-}
-
-void sendData() {
-  // 發送收到的LoRa數據作為JSON
-  String payload = "{\"lat\":\"" + receivedLat + "\",\"lon\":\"" + receivedLon + "\",\"status\":\"" + receivedStatus + "\"}";
-  server.send(200, "application/json", payload);
+void processReceivedData(String data) {
+    if (data.startsWith("Drowning Alert!")) {
+        Serial.println("Drowning alert received!");
+        Blynk.notify("Drowning alert!");
+    } else {
+        // 處理常規的位置數據
+        int latIndex = data.indexOf("LAT:");
+        int lonIndex = data.indexOf(",LONG:");
+        if (latIndex != -1 && lonIndex != -1) {
+            double lat = data.substring(latIndex + 4, lonIndex).toDouble();
+            double lon = data.substring(lonIndex + 6).toDouble();
+            Blynk.virtualWrite(V1, lat, lon, "GPS Location");
+        }
+    }
 }
